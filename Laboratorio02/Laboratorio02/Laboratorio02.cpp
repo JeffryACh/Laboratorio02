f50d10c56e47d3a4386390b66c38b3b0410ee261
JeffryACh <jeffry2811ach@gmail.com>
2025-08-12 13:26:44 -0600

    Primera version

---
+++ Laboratorio02/Laboratorio02/Laboratorio02.cpp
@@ -0,0 +1,22 @@
+/*
+* Laboratorio02
+*
+* @Authors: 
+*   - Jeffry Araya Ch.
+*   - Federick Fernadez Ca.
+*   - Juan J. Rojas
+*
+* Created: 12/08/2025 13:25
+*
+* Modified: 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+	return 0;
+}
